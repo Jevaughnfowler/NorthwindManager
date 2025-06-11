01e692c7a2475ff7e26401062d318dd69c14b23c
jevaughnfowler <jevaughnfowler@gmail.com>
2025-06-11 14:00:30 -0400

    commit to save

---
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -0,0 +1,106 @@
+package com.pluralsight.ui;
+
+import com.pluralsight.data.NorthwindDataManager;
+import com.pluralsight.models.Category;
+import com.pluralsight.models.Employee;
+
+import java.util.List;
+
+public class UserInterface {
+
+    private Employee currentEmployee;
+    private Console console;
+    private NorthwindDataManager dataManager;
+
+    public UserInterface(NorthwindDataManager dataManager) {
+        this.console = new Console();
+        this.dataManager = dataManager;
+    }
+
+
+    public void display() {
+        System.out.println("Welcome to the Northwind Manager!");
+        currentEmployee = loginUser();
+        System.out.println("Welcome, " + this.currentEmployee.getFirstName() + "!");
+
+
+        showHomeMenu();
+    }
+
+
+    private Employee loginUser() {
+        String s = console.promptForString("Just git enter to login in as Jevaughn \n");
+        Employee e = new Employee(1, "Jevaughn", "Fowler");
+        return e;
+    }
+
+    private void showHomeMenu() {
+
+
+        while (true) {
+            System.out.println();
+            System.out.println();
+
+            String[] menuOptions = {
+                    "list product categories",
+                    "list all products",
+                    "list products by category",
+                    "list products by price",
+                    "list all suppliers",
+                    "list products by supplier",
+                    "exit"
+            };
+
+            int userChoice = console.promptForOption(menuOptions);
+            switch (userChoice) {
+                case 1:
+                    listCategoriesAll();
+                    break;
+                case 2:
+                    listProductsAll();
+                    break;
+                case 3:
+                    listProductsByCategory();
+                    break;
+                case 4:
+                    listProductsByPrice();
+                    break;
+                case 5:
+                    listSuppliersAll();
+                    break;
+                case 6:
+                    listProductsBySupplier();
+                    break;
+                case 7:
+                    System.exit(0);
+            }
+
+
+            console.promptForString("Please press <ENTER> to continue...", true);
+        }
+    }
+
+    private void listProductsBySupplier() {
+    }
+
+    private void listSuppliersAll() {
+    }
+
+    private void listProductsByPrice() {
+    }
+
+    private void listProductsByCategory() {
+    }
+
+    private void listProductsAll() {
+    }
+
+    private void listCategoriesAll() {
+        List<Category> categories = dataManager.getCategories();
+        if (categories.stream().count() <= 0) {
+            System.out.println("No Categories found.");
+        } else {
+            categories.stream().forEach(c -> System.out.println(c.getCategoryName()));
+        }
+    }
+}
